038658842cb901b2bb592367fbcff72281a0f83e
Daniele Gazzola <danielegazzola2000@gmail.com>
2023-10-14 09:25:15 +0200

    09.10.1.cpp file modified - faster but still very slow mat initialization

--- DanieleGazzola/09.10.1.cpp
+++ DanieleGazzola/09.10.1.cpp
@@ -1,6 +1,11 @@
 #include <cstdlib>
 #include <iostream>
 #include <math.h>
+#include <chrono>
+#include <fstream>
+#include <unistd.h>
+#include <string>
+#include <sstream>
 #include <eigen3/Eigen/SparseCore>
 #include <eigen3/Eigen/IterativeLinearSolvers>
 #include <eigen3/unsupported/Eigen/SparseExtra>
@@ -12,20 +17,35 @@ int N;
 
 //-----------------------------SUPPORT FUNCTIONS-------------------------------//
 
-int posx(int n){
-    return (n % N) + 1;
-}
+void ram_usage(){
+    pid_t pid = getpid();
+    std::string statusFile = "/proc/" + std::to_string(pid) + "/status";
+    std::ifstream file(statusFile.c_str());
+    if (!file.is_open()) {
+        std::cerr << "Impossibile aprire il file status del processo." << std::endl;
+        return ;
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.find("VmRSS") != std::string::npos) {
+            break;
+        }
+    }
+    file.close();
 
-int posy(int n){
-    return ((n % (N * N)) / N) + 1;
-}
+    //print ram usage
+    std::istringstream iss(line);
+    std::string trash;
+    int ram;
+    iss >> trash >> ram;
 
-int posz(int n){
-    return (n / (N * N)) + 1;
-}
-
-double solution(double x, double y, double z){
-    return sin(x) * sin(y) * sin(z);
+    std::cout << "RAM: " << ram << std::endl;
+    /*
+    std::ofstream outfile;
+    outfile.open("RAM.csv", std::ios_base::app);
+    outfile << N << "," << ram << std::endl;
+    outfile.close();
+    */
 }
 
 //-----------------------------------MAIN--------------------------------------//
@@ -50,16 +70,22 @@ int main(int argc, char** argv){
 
     int size = N * N * N;
 
+    double* sin = new double[N];
+
     Eigen::SparseMatrix<double> mat(size, size);
     Eigen::VectorXd rhs(size);
     Eigen::VectorXd sol(size);
     Eigen::VectorXd exact_sol(size);
 
-//-------------------------CREATION MATRIX AND VECORS--------------------------//
+//-------------------------CREATION MATRIX AND VECTORS-------------------------//
+
+    //sin calculation
+    for(int i = 1; i <= N; i++)
+        sin[i - 1] = std::sin(i * dx);
 
     //exact_sol Time = 0
     for(int i = 0; i < size; i++)
-        exact_sol[i] = solution(posx(i) * dx, posy(i) * dx, posz(i) * dx);
+        exact_sol[i] = sin[i % N] * sin[(i % (N * N)) / N] * sin[i / (N * N)];
 
     //sol Time = 0
     for(int i = 0; i < size; i++)
@@ -74,65 +100,86 @@ int main(int argc, char** argv){
         //boundary conditions
 
         //X = 1
-        if(posx(i) == N)
-            rhs[i] -= exdiag * solution(X, posy(i) * dx, posz(i) * dx);
+        if(i % N == N - 1)
+            rhs[i] -= exdiag * std::sin(X) * sin[(i % (N * N)) / N] * sin[i / (N * N)];
 
         //Y = 1
-        if(posy(i) == N)
-            rhs[i] -= exdiag * solution(posx(i)* dx, X, posz(i) * dx);
+        if((i % (N * N)) / N == N - 1)
+            rhs[i] -= exdiag * sin[i % N] * std::sin(X) * sin[i / (N * N)];
 
         //Z = 1
-        if(posz(i) == N)
-            rhs[i] -= exdiag * solution(posx(i) * dx, posy(i) * dx, X);
+        if(i / (N * N) == N - 1)
+            rhs[i] -= exdiag * sin[i % N] * sin[(i % (N * N)) / N] * std::sin(X);
     }
+
+    auto start1 = std::chrono::high_resolution_clock::now();
 
     //mat initialization
     for (int i = 0; i < size; i++) {
 
-        //diagonal
-        mat.coeffRef(i, i) = diag;
-
-        //diagonal - 1
-        if(i > 0 && i % N != 0)
-            mat.coeffRef(i, i - 1) = exdiag;
-
-        //diagnoal + 1
-        if(i < size - 1 && (i + 1) % N != 0)
-            mat.coeffRef(i, i + 1) = exdiag;
-
-        //diagonal - N
-        if(i >= N && posy(i) != 1)
-            mat.coeffRef(i, i - N) = exdiag;
-
-        //diagonal + N
-        if(i < size - N && posy(i) != N)
-            mat.coeffRef(i, i + N) = exdiag;
-
         //diagonal - N * N
         if(i >= N * N)
-            mat.coeffRef(i, i - N * N) = exdiag;
+            mat.insert(i, i - N * N) = exdiag;
+
+        //diagonal - N
+        if(i >= N)
+            if ((i % (N * N)) / N != 0)
+                mat.insert(i, i - N) = exdiag;
+
+        //diagonal - 1
+        if(i >= 1)
+            if (i % N != 0)
+                mat.insert(i, i - 1) = exdiag;
+
+        //diagonal
+        mat.insert(i, i) = diag;
+
+        //diagnoal + 1
+        if(i < size - 1)
+            if (i % N != N - 1)
+                mat.insert(i, i + 1) = exdiag;
+
+        //diagonal + N
+        if(i < size - N)
+            if ((i % (N * N)) / N != N - 1)
+                mat.insert(i, i + N) = exdiag;
 
         //diagonal + N * N
         if(i < size - N * N)
-            mat.coeffRef(i, i + N * N) = exdiag;
-
+            mat.insert(i, i + N * N) = exdiag;
     }
+
+    auto end1 = std::chrono::high_resolution_clock::now();
 
     mat.makeCompressed();
 
 //-----------------------------COMPUTE SOLUTION---------------------------------//
 
+    auto start2 = std::chrono::high_resolution_clock::now();
+
     Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper> solver;
     solver.compute(mat);
     sol = solver.solve(rhs);
 
-//-----------------------------ERROR AND OUTPUT---------------------------------//
+    auto end2 = std::chrono::high_resolution_clock::now();
+
+//----------------------------ERROR, TIME, OUTPUT-------------------------------//
 
     for(int i = 0; i < size; i++)
         error += (sol[i] - exact_sol[i]) * (sol[i] - exact_sol[i]);
-    error = sqrt(error) / size;
+    error = sqrt(error);
 
-    std::cout << "Error: " << error << std::endl;
+    auto elapsed1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - start1);
+    auto elapsed2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end2 - start2);
+
+    std::cout << "Error: " << error / size << std::endl;
+    std::cout << "Time for matrix initialization: " << elapsed1.count() / pow(10, 9) / size << " s / N^3" << std::endl;
+    std::cout << "Time for solution: " << elapsed2.count() / pow(10, 9) / size << " s / N^3" << std::endl;
+
+    // Controllo RAM
+    ram_usage();
+
+    delete[] sin;
 
     return 0;
 }
